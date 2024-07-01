dca582747df8661204ba9f88a8c0b2d5b0d9d2f5
Ivan Elorm Fugar <ivanquayson28@gmail.com>
2024-07-01 11:34:31 +0000

    Update

---
+++ app.py
@@ -0,0 +1,20 @@
+# Importing modules
+from reportlab.platypus import SimpleDocTemplate, Table, Paragraph, TableStyle
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.styles import getSampleStyleSheet
+
+# Data to be displayed as tables
+data = [
+    ["Date", "Name", "Subscription", "Price($)"],
+    [
+        "1/7/2024",
+        "Python and Django Development Course",
+        "1 year",
+        "400/-"
+    ],
+    ["1/7/2024", "Udemy Classes: Live Session", "3 months", "275/-"],
+    ["Sub Total", "", "", "675/-"],
+    ["Discount", "", "", "50/-"],
+    ["Total", "", "", "625/-"],
+]
