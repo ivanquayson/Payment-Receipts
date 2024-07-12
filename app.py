9756cb491824e723795f8a51b66cb299bce9e0c5
Ivan Elorm Fugar <ivanquayson28@gmail.com>
2024-07-12 13:43:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,20 @@ from reportlab.lib import colors
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.styles import getSampleStyleSheet
 
-# Data to be displayed as tables
-data = [
-    ["Date", "Name", "Subscription", "Price($)"],
-    [
-        "1/7/2024",
-        "Python and Django Development Course",
-        "1 year",
-        "400/-"
-    ],
-    ["1/7/2024", "Udemy Classes: Live Session", "3 months", "275/-"],
-    ["Sub Total", "", "", "675/-"],
-    ["Discount", "", "", "50/-"],
-    ["Total", "", "", "625/-"],
-]
 
-pdf = SimpleDocTemplate( "receipt.pdf" , pagesize = A4 )
+def get_receipts_details():
+    details = {}
 
-styles = getSampleStyleSheet() 
+    # Company details
+    details["company_name"] = input("Enter company name: ")
+    details["company_address"] = input("Enter company address: ")
+    details["company_city"] = input("Enter company city and state: ")
+
+    # Bill-to details
+    details["bill_to_name"] = input("Enter Bill To name: ")
+    details["bill_to_address"] = input("Enter Bill To address: ")
+    details["bill_to_city"] = input("Enter Bill To city and state: ")
+
+    # Receipt details
+    details["receipt_number"] = input("Enter receipt number: ")
+    details["receipt_date"] = input("Enter receipt date (DD-MM-YYYY): ")
