5173d177dabad158a82e8580227777b7b4e5d8e1
Ivan Elorm Fugar <ivanquayson28@gmail.com>
2024-07-16 10:20:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -49,3 +49,60 @@ def get_receipt_details():
 def generate_receipt(details, output_path):
     doc = SimpleDocTemplate(output_path, pagesize=letter)
     elements = []
+
+    styles = getSampleStyleSheet()
+
+    if 'Title' not in styles:
+        styles.add(ParagraphStyle(name='Title', fontSize=18, leading=22, spaceAfter=12))
+    else:
+        styles['Title'].fontSize = 18
+        styles['Title'].leading = 22
+        styles['Title'].spaceAfter = 12
+
+    if 'Heading' not in styles:
+        styles.add(ParagraphStyle(name='Heading', fontSize=12, leading=14, spaceAfter=8))
+    else:
+        styles['Heading'].fontSize = 12
+        styles['Heading'].leading = 14
+        styles['Heading'].spaceAfter = 8
+
+    if 'Body' not in styles:
+        styles.add(ParagraphStyle(name='Body', fontSize=10, leading=12, spaceAfter=6))
+    else:
+        styles['Body'].fontSize = 10
+        styles['Body'].leading = 12
+        styles['Body'].spaceAfter = 6
+
+    # Company details
+    company_info = f"""
+    <b>{details["company_name"]}</b><br/>
+    {details["company_address"]}<br/>
+    {details["company_city"]}<br/>
+    """
+    elements.append(Paragraph(company_info, styles['Body']))
+
+    # Spacer
+    elements.append(Spacer(1, 12))
+
+    # Bill To details
+    bill_to_info = f"""
+    <b>Billed To</b><br/>
+    {details["bill_to_name"]}<br/>
+    {details["bill_to_address"]}<br/>
+    {details["bill_to_city"]}<br/>
+    """
+    elements.append(Paragraph(bill_to_info, styles['Body']))
+
+    # Spacer
+    elements.append(Spacer(1, 12))
+
+    # Receipt details
+    receipt_info = f"""
+    <b>Receipt #</b> {details["receipt_number"]}<br/>
+    <b>Receipt date</b> {details["receipt_date"]}<br/>
+    """
+    elements.append(Paragraph(receipt_info, styles['Body']))
+
+    # Spacer
+    elements.append(Spacer(1, 24))
+
